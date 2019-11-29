8a664de49976cfaaca73a573c790cac894f690e8
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 00:20:10 +0300

    Scripted auto-commit on change (2019-11-30 00:20:10) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/Handler.java
+++ lab6/src/main/java/com/example/Laba/Handler.java
@@ -1,4 +1,16 @@
 package com.example.Laba;
 
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+
 public class Handler {
+    ZooKeeper zoo;
+    String path;
+    ActorRef storage;
+
+    Handler(ZooKeeper zoo, ActorRef storage, String path){
+        this.zoo = zoo;
+        this.storage = storage;
+        this.path = path;
+    }
 }
