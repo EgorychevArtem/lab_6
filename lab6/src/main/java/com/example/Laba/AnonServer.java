883e8c05decc5e7a63c1a72a1284e5195088caa0
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 01:39:38 +0300

    Scripted auto-commit on change (2019-11-30 01:39:38) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/AnonServer.java
+++ lab6/src/main/java/com/example/Laba/AnonServer.java
@@ -57,6 +57,6 @@ public class AnonServer {
 
     public Request createServerRequest(String servurl, String url, int count){
         return http.prepareGet(servurl).addQueryParam("url", url)
-                .addQueryParam("count", count).build();
+                .addQueryParam("count", Integer.toString(count)).build();
     }
 }
