811b4c1e79d5334a8b85f31a9a765f8ebb1b93c7
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 01:31:48 +0300

    Scripted auto-commit on change (2019-11-30 01:31:48) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/AnonServer.java
+++ lab6/src/main/java/com/example/Laba/AnonServer.java
@@ -4,6 +4,10 @@ import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Request;
+import org.asynchttpclient.Response;
+
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
 
@@ -25,6 +29,19 @@ public class AnonServer {
                                 handleGetWithUrlCount(url, Integer.parseInt(count)))
                                 )
                         )
-        )
+        );
     }
+
+    public Route handleGetWithUrlCount(String url, int count){
+        CompletionStage<Response> result;
+        if (count == 0){
+            result =
+        }
+    }
+
+
+    public CompletionStage<Response> Get(Request req){
+        
+    }
+
 }
