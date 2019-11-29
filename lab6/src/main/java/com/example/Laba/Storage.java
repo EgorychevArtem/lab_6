d75be6447420d9011287689177f214a5b810d900
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 00:28:37 +0300

    Scripted auto-commit on change (2019-11-30 00:28:37) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/Storage.java
+++ lab6/src/main/java/com/example/Laba/Storage.java
@@ -1,4 +1,13 @@
 package com.example.Laba;
 
+import java.util.List;
+import java.util.Random;
+
 public class Storage {
+    Random random = new Random();
+    List<String> storage;
+
+    Storage(){
+       
+    }
 }
