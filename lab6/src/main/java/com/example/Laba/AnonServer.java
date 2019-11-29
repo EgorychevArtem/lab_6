0756ddaf3173f398bcec69b4a3027c28c7e61888
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 01:19:03 +0300

    Scripted auto-commit on change (2019-11-30 01:19:03) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/AnonServer.java
+++ lab6/src/main/java/com/example/Laba/AnonServer.java
@@ -5,7 +5,12 @@ import org.apache.zookeeper.ZooKeeper;
 import org.asynchttpclient.AsyncHttpClient;
 
 public class AnonServer {
-    
+    AsyncHttpClient http;
+    ActorRef storage;
+    ZooKeeper zoo;
     public AnonServer(ActorRef storage, AsyncHttpClient httpClient, ZooKeeper zoo) {
+        this.http = httpClient;
+        this.storage = storage;
+        this.zoo = zoo;
     }
 }
