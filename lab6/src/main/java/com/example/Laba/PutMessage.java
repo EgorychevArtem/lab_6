9ec61623b890f3e0c9ff4a4003fdb2c7add4da23
Egorychev Artem <temych_2000@mail.ru>
2019-11-30 00:32:32 +0300

    Scripted auto-commit on change (2019-11-30 00:32:32) by gitwatch.sh

--- lab6/src/main/java/com/example/Laba/PutMessage.java
+++ lab6/src/main/java/com/example/Laba/PutMessage.java
@@ -1,4 +1,15 @@
 package com.example.Laba;
 
+import java.util.List;
+
 public class PutMessage {
+    List<String> servers;
+
+    public PutMessage(List<String> servers){
+        this.servers = servers;
+    }
+
+    public List<String> getServers() {
+        return servers;
+    }
 }
